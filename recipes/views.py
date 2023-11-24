bfe15fc180aa352a9f5f939e20287d324d3e3e75
adalbertobengaly <adalbertobengaly@gmail.com>
2023-11-24 17:55:31 -0300

    Removido contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -7,11 +7,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Adalberto Bengaly',
     })
-
-
-def sobre(request):
-    return render(request, 'recipes/sobre.html')
-
-
-def contato(request):
-    return render(request, 'recipes/contato.html')
