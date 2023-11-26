485ba3afa5e7a907d7d9abf9352d70e31d80f8c5
adalbertobengaly <adalbertobengaly@gmail.com>
2023-11-26 12:17:49 -0300

    Uma nova URL para uma nova View - sobre URLs no Django

--- recipes/views.py
+++ recipes/views.py
@@ -7,3 +7,9 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'name': 'Adalberto Bengaly',
     })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Adalberto Bengaly',
+    })
