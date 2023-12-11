6c99bbf0a8dab263bcee7af91c917467c698b36d
adalbertobengaly <adalbertobengaly@gmail.com>
2023-12-11 17:27:03 -0300

    Levantando erro 404 se o termo de busca não for enviado

--- recipes/tests/test_recipe_views.py
+++ recipes/tests/test_recipe_views.py
@@ -124,3 +124,8 @@ class RecipeViewsTest(RecipeTestBase):
         url = reverse('recipes:search')
         response = self.client.get(url)
         self.assertTemplateUsed(response, 'recipes/pages/search.html')
+
+    def test_recipe_search_raises_404_if_no_search_term(self):
+        url = reverse('recipes:search')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
