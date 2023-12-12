a894e26e1d6a8816f8a952f40b1bb370b0d84347
adalbertobengaly <adalbertobengaly@gmail.com>
2023-12-12 18:34:15 -0300

    Reutilizando a paginação com make_pagination

--- utils/pagination.py
+++ utils/pagination.py
@@ -1,4 +1,5 @@
 import math
+from django.core.paginator import Paginator
 
 
 def make_pagination_range(
@@ -21,6 +22,7 @@ def make_pagination_range(
         start_range = start_range - abs(total_pages - stop_range)
 
     pagination = page_range[start_range:stop_range]
+
     return {
         'pagination': pagination,
         'page_range': page_range,
@@ -32,3 +34,21 @@ def make_pagination_range(
         'first_page_out_of_range': current_page > middle_range,
         'last_page_out_of_range': stop_range < total_pages,
     }
+
+
+def make_pagination(request, queryset, per_page, qty_pages=4):
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    paginator = Paginator(queryset, per_page)
+    page_object = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        qty_pages,
+        current_page
+    )
+
+    return page_object, pagination_range
