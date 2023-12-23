58a3a636eca66b1ccbde017aff1bd3500d4ae6fa
adalbertobengaly <adalbertobengaly@gmail.com>
2023-12-23 16:34:21 -0300

    Movendo repetições para setUp e tearDown

--- tests/functional_tests/test_recipe_home_page.py
+++ tests/functional_tests/test_recipe_home_page.py
@@ -5,14 +5,22 @@ from utils.browser import make_chrome_browser
 from selenium.webdriver.common.by import By
 
 
-class RecipeHomePageFunctionalTest(StaticLiveServerTestCase):
+class RecipeBaseFunctionalTestCase(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
     def sleep(self, seconds=5):
         time.sleep(seconds)
 
-    def test_the_test(self):
-        browser = make_chrome_browser()
-        browser.get(self.live_server_url)
-        self.sleep(6)
-        body = browser.find_element(By.TAG_NAME, 'body')
+
+class RecipeHomePageFunctionalTest(RecipeBaseFunctionalTestCase):
+
+    def test_recipe_home_page_with_recipes_not_found_message(self):
+        self.browser.get(self.live_server_url)
+        body = self.browser.find_element(By.TAG_NAME, 'body')
         self.assertIn('No recipes found here 😮‍💨', body.text)
-        browser.quit()
--- utils/browser.py
+++ utils/browser.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from time import sleep
+import os
 
 
 def make_chrome_browser(*options):
@@ -8,6 +9,9 @@ def make_chrome_browser(*options):
     if options is not None:
         for option in options:
             chrome_options.add_argument(option)
+
+    if os.environ.get('SELENIUM_HEADLESS') == '1':
+        chrome_options.add_argument('--headless')
 
     browser = webdriver.Chrome(options=chrome_options)
 
