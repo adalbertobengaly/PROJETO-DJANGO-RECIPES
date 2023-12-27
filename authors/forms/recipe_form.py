f6aaed378a657268884f2a151144153be96006b2
adalbertobengaly <adalbertobengaly@gmail.com>
2023-12-27 11:56:07 -0300

    Toques finais no layout do formulário de criação de receitas

--- authors/forms/recipe_form.py
+++ authors/forms/recipe_form.py
@@ -8,7 +8,6 @@ class AuthorRecipeForm(forms.ModelForm):
         super().__init__(*args, **kwargs)
 
         add_attr(self.fields.get('preparation_steps'), 'class', 'span-2')
-        add_attr(self.fields.get('cover'), 'class', 'span-2')
 
     class Meta:
         model = Recipe
@@ -22,3 +21,24 @@ class AuthorRecipeForm(forms.ModelForm):
             'preparation_steps',
             'cover',
         )
+
+        widgets = {
+            'cover': forms.FileInput(
+                attrs={
+                    'class': 'span-2'
+                }
+            ),
+            'servings_unit': forms.Select(
+                choices=(
+                    ('Porções', 'Porções'),
+                    ('Pessoas', 'Pessoas'),
+                    ('Pratos', 'Pratos'),
+                ),
+            ),
+            'preparation_time_unit': forms.Select(
+                choices=(
+                    ('Minutos', 'Minutos'),
+                    ('Hora(s)', 'Hora(s)'),
+                ),
+            ),
+        }
