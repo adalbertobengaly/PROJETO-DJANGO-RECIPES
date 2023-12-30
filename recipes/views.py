9a6927491a30dc091c76e6029344ff8caa4adb3e
adalbertobengaly <adalbertobengaly@gmail.com>
2023-12-29 21:24:40 -0300

    Home ListView

--- recipes/views.py
+++ recipes/views.py
@@ -42,6 +42,10 @@ class RecipeListViewBase(ListView):
         return context
 
 
+class RecipeListViewHome(RecipeListViewBase):
+    template_name = 'recipes/pages/home.html'
+
+
 def home(request):
     recipes = Recipe.objects.filter(
         is_published=True
