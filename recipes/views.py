da12c40f531ac287942934c773bb537b2e24609d
adalbertobengaly <adalbertobengaly@gmail.com>
2024-01-03 15:06:24 -0300

    Evitando consultas desnecessárias com prefetch_related

--- recipes/views.py
+++ recipes/views.py
@@ -26,6 +26,7 @@ class RecipeListViewBase(ListView):
             is_published=True,
         )
         query_set = query_set.select_related('author', 'category')
+        # query_set = query_set.prefetch_related('author', 'category')
         return query_set
 
     def get_context_data(self, *args, **kwargs):
