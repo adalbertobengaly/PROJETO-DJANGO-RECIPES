e54ee436e8dc4ba5185b1ce1d7299448e430f006
adalbertobengaly <adalbertobengaly@gmail.com>
2024-01-06 15:54:34 -0300

    Tag app: criando a relação com GenericRelation e cadastrando na admin do Django

--- recipes/models.py
+++ recipes/models.py
@@ -4,6 +4,8 @@ from django.urls import reverse
 from django.utils.text import slugify
 from django.db.models import F, Value
 from django.db.models.functions import Concat
+from django.contrib.contenttypes.fields import GenericRelation
+from tag.models import Tag
 
 
 class Category(models.Model):
@@ -53,6 +55,7 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+    tags = GenericRelation(Tag, related_query_name='recipes')
 
     def __str__(self):
         return self.title
