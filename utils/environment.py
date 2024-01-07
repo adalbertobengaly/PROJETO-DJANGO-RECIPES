c1bb15340ab4f5edf028c46a7ba9e5c5547be505
adalbertobengaly <adalbertobengaly@gmail.com>
2024-01-06 21:04:52 -0300

    ALLOWED_HOSTS e CSRF_TRUSTED_ORIGINS

---
+++ utils/environment.py
@@ -0,0 +1,19 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+def get_env_variable(variable_name, default_value=''):
+    return os.environ.get(variable_name, default_value)
+
+
+def parse_comma_sep_str_to_list(comma_sep_str):
+    if not comma_sep_str or not isinstance(comma_sep_str, str):
+        return []
+    return [string.strip() for string in comma_sep_str.split(',')]
+
+
+if __name__ == '__main__':
+    print(parse_comma_sep_str_to_list(get_env_variable('ALLOWED_HOSTS')))
+    print(parse_comma_sep_str_to_list(
+        get_env_variable('CSRF_TRUSTED_ORIGINS')))
