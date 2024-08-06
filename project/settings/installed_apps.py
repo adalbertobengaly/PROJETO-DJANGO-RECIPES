e53d5ebd7e0b2a501005b1c47eab3c10d0f184ce
Adalberto Bengaly <adalbertobengaly@gmail.com>
2024-08-06 20:51:29 -0300

    Adicionado Django Rest Framework, requirements.txt atualizado.

--- project/settings/installed_apps.py
+++ project/settings/installed_apps.py
@@ -1,11 +1,15 @@
 # Application definition
 INSTALLED_APPS = [
+    # Django
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Django Rest Framework
+    'rest_framework',
+    # Apps
     'recipes',
     'authors',
     'tag',
