49eb5afa9df2a6d4dfbb51527084a044aaad27e9
Adalberto Bengaly <adalbertobengaly@gmail.com>
2024-08-07 21:28:11 -0300

    Sobre os métodos HTTP ou request.method no Django Rest Framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -1,19 +1,24 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer, TagSerializer
 from django.shortcuts import get_object_or_404, get_list_or_404
 from tag.models import Tag
 
-@api_view()
+@api_view(http_method_names=['get', 'post'])
 def recipe_api_list(request):
-    recipes = Recipe.objects.get_published()[:10]
-    serializer = RecipeSerializer(
-        instance=recipes, 
-        many=True,
-        context={'request': request}
-        )
-    return Response(serializer.data)
+    if request.method == 'GET':
+        recipes = Recipe.objects.get_published()[:10]
+        serializer = RecipeSerializer(
+            instance=recipes, 
+            many=True,
+            context={'request': request}
+            )
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        return Response('POST', status=status.HTTP_201_CREATED)
+
 
 @api_view()
 def recipe_api_detail(request, pk):
