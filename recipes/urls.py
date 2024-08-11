322f9d1d74db947262707600c23aea2bb92f4798
Adalberto Bengaly <adalbertobengaly@gmail.com>
2024-08-11 14:29:55 -0300

    Router, urlpatterns, path e include no urls.py do Django Rest Framework

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,16 @@
-from django.urls import path
+from django.urls import path, include
+
 from recipes import views
+from rest_framework.routers import SimpleRouter
 
 app_name = 'recipes'
+
+recipe_api_v2_router = SimpleRouter()
+recipe_api_v2_router.register(
+    'recipes/api/v2', 
+    views.RecipeAPIv2ViewSet,
+    basename='recipes-api',
+)
 
 urlpatterns = [
     path('', views.RecipeListViewHome.as_view(), name="home"),
@@ -30,25 +39,11 @@ urlpatterns = [
     ),
     path('recipes/theory/', views.theory, name="theory"),
     path(
-        'recipes/api/v2/', 
-        views.RecipeAPIv2ViewSet.as_view({
-            'get': 'list',
-            'post': 'create',
-        }), 
-        name="recipe_api_v2"
-    ),
-    path(
-        'recipes/api/v2/<int:pk>', 
-        views.RecipeAPIv2ViewSet.as_view({
-            'get': 'retrieve',
-            'patch': 'partial_update',
-            'delete': 'destroy'
-        }), 
-        name="recipe_api_v2_detail"
-    ),
-    path(
         'recipes/api/v2/tag/<int:pk>', 
         views.tag_api_detail, 
         name="recipe_api_v2_tag"
     ),
+    path('', include(recipe_api_v2_router.urls)),
 ]
+
+# urlpatterns += recipe_api_v2_router.urls
