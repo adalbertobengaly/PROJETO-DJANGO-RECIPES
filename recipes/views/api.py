a9a27323b091cd3a9eebc742c29d4f3f28630db3
Adalberto Bengaly <adalbertobengaly@gmail.com>
2024-08-11 22:52:42 -0300

    Conhecendo as Permission Classes no Django Rest Framework

--- recipes/views/api.py
+++ recipes/views/api.py
@@ -5,6 +5,7 @@ from recipes.models import Recipe
 from recipes.serializers import RecipeSerializer, TagSerializer
 from django.shortcuts import get_object_or_404
 from rest_framework.viewsets import ModelViewSet
+from rest_framework.permissions import IsAuthenticated
 from tag.models import Tag
 
 
@@ -16,6 +17,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     queryset = Recipe.objects.get_published()
     serializer_class = RecipeSerializer
     pagination_class = RecipeAPIv2Pagination
+    permission_classes = [IsAuthenticated, ]
 
     def get_serializer_class(self):
         return super().get_serializer_class()
@@ -32,7 +34,7 @@ class RecipeAPIv2ViewSet(ModelViewSet):
     def get_queryset(self):
         qs =super().get_queryset()
 
-        category_id = self.request.query_params.get('category_id', None)
+        category_id = self.request.query_params.get('category_id', '')
 
         if category_id != '' and category_id.isnumeric():
             qs = qs.filter(category_id=category_id)
