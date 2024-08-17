3c4919b8895071c93b22603477a1968da9a6df62
Adalberto Bengaly <adalbertobengaly@gmail.com>
2024-08-17 14:05:10 -0300

    Usando "response.data" para analisar a resposta retornada por self.client

--- recipes/tests/test_recipe_api.py
+++ recipes/tests/test_recipe_api.py
@@ -1,6 +1,7 @@
 from django.urls import reverse
 from rest_framework import test
 from recipes.tests.test_recipe_base import RecipeMixin
+from unittest.mock import patch
 
 class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
     def test_recipe_api_list_return_status_code_200(self):
@@ -9,4 +10,15 @@ class RecipeAPIv2Test(test.APITestCase, RecipeMixin):
         self.assertEqual(
             response.status_code,
             200
+        )
+
+    @patch('recipes.views.api.RecipeAPIv2Pagination.page_size', new=7)
+    def test_recipe_api_list_loads_correct_number_of_recipes(self):
+        wanted_number_of_recipes = 7
+        self.make_recipes_in_batch(qtd=wanted_number_of_recipes)
+        response = self.client.get(reverse('recipes:recipes-api-list'))
+        qtd_of_loaded_recipes = len(response.data.get('results'))
+        self.assertEqual(
+            wanted_number_of_recipes,
+            qtd_of_loaded_recipes
         )
